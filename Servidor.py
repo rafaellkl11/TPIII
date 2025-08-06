3d54e24651922e13c7a0c6e6db383d7656584d4b
rafaellkl11 <rafaelhenriqueif@gmail.com>
2025-08-05 22:25:11 -0300

    3°commit

--- Servidor.py
+++ Servidor.py
@@ -26,6 +26,7 @@ def servidor():
 
 		if(temp<15 or temp>35):
 			res="Temperatura fora do padrão!"
+			arquivo = open('dados.csv', 'a')
 			connectionSocket.send(res.encode())
 		else:
 			res="Dados armazenados com sucesso!"
@@ -36,8 +37,36 @@ def resultados():
 	while True:
 		time.sleep(10)
 		arquivo = open("dados.csv","r")
+		ids = []
 		for dado in arquivo:
-			print(dado[0])
+			dado = dado.split(";")
+			if (ids == None):
+				ids.append(dado[0])
+			else:
+				if (not(dado[0] in ids)):
+					ids.append(dado[0])
+		arquivo.close()
+		
+		ids.sort()
+		print(ids)
+
+		i=0
+
+		
+		while (i!=len(ids)):
+			j = 0
+			soma = 0
+			media = 0
+			arquivo = open ("dados.csv","r")
+			for dado in arquivo:
+				dado = dado.split(";")
+				if (ids[i] == dado[0]):
+					soma = soma + int(dado[1])
+					j = j + 1
+			media = soma / j
+			print ("id: ",ids[i],"|","media: ",media)
+			arquivo.close()
+			i = i + 1
 
 servidor1 = Thread(target=servidor)
 resultados2 = Thread(target=resultados)
